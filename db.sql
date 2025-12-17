a338de68ac480a5e4ab5a00c9756b9f0f4adc12a
Randy <raoulrandy3@gmail.com>
2025-12-17 14:56:10 +0300

    fix the database name

--- db.sql
+++ db.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE "mini_dish_bd";
+CREATE DATABASE "mini_dish_db";
 
-CREATE USER "mini_dish_bd_manager" WITH PASSWORD '123456';
+CREATE USER "mini_dish_db_manager" WITH PASSWORD '123456';
 
-GRANT CREATE ON DATABASE "mini_dish_bd" TO "mini_dish_bd_manager";
+GRANT CREATE ON DATABASE "mini_dish_db" TO "mini_dish_db_manager";
 
-\c mini_dish_bd
+\c mini_dish_db
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "mini_dish_bd_manager";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "mini_dish_db_manager";
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "mini_dish_bd_manager";
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "mini_dish_db_manager";
