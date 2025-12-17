2211138433e993320ed00aa5363e50b3bd57dd88
Randy <raoulrandy3@gmail.com>
2025-12-17 13:51:56 +0300

    fix database and user name

--- init_db.sql
+++ init_db.sql
@@ -1,7 +1,12 @@
-CREATE DATABASE "dish_management_db";
-CREATE USER "dish_manager_user" WITH PASSWORD '123456';
-GRANT CREATE ON DATABASE "dish_management_db" TO "dish_manager_user";
-\c dish_management_db
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "dish_manager_user";
+CREATE DATABASE "mini_dish_bd";
+
+CREATE USER "mini_dish_bd_manager" WITH PASSWORD '123456';
+
+GRANT CREATE ON DATABASE "mini_dish_bd" TO "mini_dish_bd_manager";
+
+\c mini_dish_bd
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "mini_dish_bd_manager";
+
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "dish_manager_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "mini_dish_bd_manager";
