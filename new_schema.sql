125fc5c2cc4154a39a036dbb8f6ae8aa1131b44a
Randy <raoulrandy3@gmail.com>
2026-01-16 12:17:28 +0300

    add the new schema to the database

---
+++ new_schema.sql
@@ -0,0 +1,17 @@
+ALTER TABLE "Ingredient" DROP CONSTRAINT fk_id_dish, DROP COLUMN id_dish;
+
+CREATE TYPE "unit_type" AS ENUM (
+    'PCS',
+    'KG',
+    'L'
+);
+
+CREATE TABLE "DishIngredient" (
+    id SERIAL PRIMARY KEY,
+    id_dish INT,
+    CONSTRAINT fk_id_dish FOREIGN KEY (id_dish) REFERENCES "Dish"(id),
+    id_ingredient INT,
+    CONSTRAINT fk_id_ingredient FOREIGN KEY (id_ingredient) REFERENCES "Ingredient"(id),
+    quantity_required NUMERIC(10,2),
+    unit "unit_type"
+);
